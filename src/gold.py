edf4120bdbfc18e455cd1f2d3610022331c7ee5c
derekvarghese <derekvarghese@gmail.com>
2026-08-24 22:48:10 +0530

    sales_summary table transformation

--- src/gold.py
+++ src/gold.py
@@ -46,6 +46,128 @@ def transform_customer_sales(cursor):
         )
 
 
+def transform_product_performance(cursor):
+    cursor.execute("DELETE FROM gold.product_performance")
+
+    cursor.execute("""
+        with ProductMetrics as (
+            select
+                StockCode,
+                sum(Quantity) as TotalQuantitySold,
+                sum(Quantity * UnitPrice) as TotalRevenue,
+                count(distinct InvoiceNo) as TotalOrders,
+                count(distinct CustomerID) as UniqueCustomers,
+                min(InvoiceDate) as FirstSaleDate,
+                max(InvoiceDate) as LastSaleDate
+            from silver.online_retail
+            group by StockCode
+            ),
+            DescriptionCounts AS (
+                SELECT
+                    StockCode,
+                    Description,
+                    COUNT(*) AS DescriptionCount,
+                    ROW_NUMBER() OVER (
+                        PARTITION BY StockCode
+                        ORDER BY COUNT(*) DESC
+                    ) AS rn
+                FROM silver.online_retail
+                WHERE Description IS NOT NULL
+                GROUP BY
+                    StockCode,
+                    Description
+            ),
+            CleanDescriptions AS (
+                SELECT
+                    StockCode,
+                    Description
+                FROM DescriptionCounts
+                WHERE rn = 1
+            )
+            SELECT
+                pm.StockCode,
+                cd.Description,
+                pm.TotalQuantitySold,
+                pm.TotalRevenue,
+                pm.TotalOrders,
+                pm.UniqueCustomers,
+                pm.FirstSaleDate,
+                pm.LastSaleDate
+            FROM ProductMetrics pm
+            LEFT JOIN CleanDescriptions cd
+                ON pm.StockCode = cd.StockCode
+    """)
+    product_performance = cursor.fetchall()
+
+    insert_product_performance_query = """
+        INSERT INTO gold.product_performance(
+            StockCode,
+            Description,
+            TotalQuantitySold,
+            TotalRevenue,
+            TotalOrders,
+            UniqueCustomers,
+            FirstSaleDate,
+            LastSaleDate    
+        )
+        VALUES (?,?,?,?,?,?,?,?)
+    """
+    for product in product_performance:
+        cursor.execute(
+            insert_product_performance_query,
+            product[0],
+            product[1],
+            product[2],
+            product[3],
+            product[4],
+            product[5],
+            product[6],
+            product[7],
+        )
+
+
+def transform_sales_summary(cursor):
+    cursor.execute("DELETE FROM gold.sales_summary")
+
+    cursor.execute("""
+        select
+            cast(datetrunc(month, InvoiceDate) as date) as SalesMonth,
+            sum(Quantity * UnitPrice) as TotalSales,
+            count(distinct InvoiceNo) as TotalOrders,
+            sum(Quantity) as TotalQuantitySold,
+            (sum(Quantity * UnitPrice) / count(distinct InvoiceNo)) as AverageOrderValue,
+            min(InvoiceDate) as FirstSaleDate,
+            max(InvoiceDate) as LastSaleDate
+        from silver.online_retail
+        group by cast(datetrunc(month, InvoiceDate) as date)
+    """)
+    sales_summary = cursor.fetchall()
+
+    insert_sales_summary_query = """
+        INSERT INTO gold.sales_summary(
+            SalesMonth,
+            TotalSales,
+            TotalOrders,
+            TotalQuantitySold,
+            AverageOrderValue,
+            FirstSaleDate,
+            LastSaleDate
+        )
+        VALUES (?,?,?,?,?,?,?)
+    """
+    for summary in sales_summary:
+        cursor.execute(
+            insert_sales_summary_query,
+            summary[0],
+            summary[1],
+            summary[2],
+            summary[3],
+            summary[4],
+            summary[5],
+            summary[6],
+        )
+
+
 def main():
     conn = get_connection()
     cursor = conn.cursor()
@@ -53,6 +175,14 @@ def main():
     transform_customer_sales(cursor)
     conn.commit()
     print("Customer sales summary moved to Gold successfully")
+
+    transform_product_performance(cursor)
+    conn.commit()
+    print("Product performance moved to Gold successfully")
+
+    transform_sales_summary(cursor)
+    conn.commit()
+    print("Sales summary moved to Gold successfully")
 
     cursor.close()
     conn.close()
